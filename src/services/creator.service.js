3488d475764d67baa296a001e4b8b97b2f280f09
Yash Verma <helloyashverma@gmail.com>
2025-10-09 20:18:19 +0530

    add new fix

--- src/services/creator.service.js
+++ src/services/creator.service.js
@@ -1,40 +1,48 @@
 import { getDb } from "../utils/mongoDb.js";
 
 export const getFullCreatorByUsername = async (username) => {
-  const db = getDb();
-  const creator = await db
-    .collection("creators")
-    .aggregate([
-      {
-        $match: {
-          username,
+  try {
+    const db = getDb();
+    const creator = await db
+      .collection("creators")
+      .aggregate([
+        {
+          $match: {
+            username,
+          },
         },
-      },
-      {
-        $lookup: {
-          from: "configs",
-          localField: "config",
-          foreignField: "_id",
-          as: "config",
+        {
+          $lookup: {
+            from: "configs",
+            localField: "config",
+            foreignField: "_id",
+            as: "config",
+          },
         },
-      },
-      {
-        $unwind: {
-          path: "$config",
-          preserveNullAndEmptyArrays: true,
+        {
+          $unwind: {
+            path: "$config",
+            preserveNullAndEmptyArrays: true,
+          },
         },
-      },
-      {
-        $project: {
-          username: 1,
-          image: 1,
-          banner: 1,
-          creator_id: 1,
-          email: 1,
-          config: 1,
+        {
+          $project: {
+            username: 1,
+            image: 1,
+            banner: 1,
+            creator_id: 1,
+            email: 1,
+            config: 1,
+          },
         },
-      },
-    ])
-    .toArray();
-  return creator[0];
+      ])
+      .toArray();
+    if (creator.length === 0) {
+      return null;
+    }
+    return creator[0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
