0332be154a2c6839aa791ef965fbcdda547cfade
Yash Verma <helloyashverma@gmail.com>
2025-10-10 05:20:07 +0530

    add new fix

--- src/controllers/event.controllers.js
+++ src/controllers/event.controllers.js
@@ -46,4 +46,38 @@ export const createEventGet = catchAsync(async (req, res, next) => {
   res.json(new ApiResponse(200, "Event created successfully"));
 });
 
-export const createEventPost = catchAsync(async (req, res) => {});
+export const createEventPost = catchAsync(async (req, res) => {
+  const validationResult = paramsSchema.safeParse(req.query);
+
+  if (!validationResult.success) {
+    throw new ApiError(400, `Validation failed params are not valid`);
+  }
+
+  // const user_ip = parseIp(req);
+  const user_ip = "49.43.163.126";
+
+  if (!user_ip) {
+    throw new ApiError(400, `Invalid or missing user IP`);
+  }
+
+  const utm_params = Object.entries(req.query)
+    .filter(([key]) => key.startsWith("utm_"))
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
+  createEvent({
+    event_type: req.query.event_type,
+    event_name: req.query.event_name,
+    event_data: req.body,
+    url: req.get("X-Current-URL"),
+    path: req.get("X-Current-Path"),
+    visitor_id: req.query.visitor_id,
+    utm_params,
+    user_agent: req.useragent,
+    user_ip,
+  });
+
+  res.json(new ApiResponse(200, "Event created successfully"));
+});
