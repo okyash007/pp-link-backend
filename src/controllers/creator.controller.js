61a2443c5145512a93939a2f23be521ad35cae52
Yash Verma <helloyashverma@gmail.com>
2025-11-22 02:39:57 +0530

    add new chnages

--- src/controllers/creator.controller.js
+++ src/controllers/creator.controller.js
@@ -1,30 +1,35 @@
-import { getFullCreatorByUsername, getCreatorWithOverlayByUsername, getCreatorWithLinkTreeByUsername } from "../services/creator.service.js";
+import {
+  getFullCreatorByUsername,
+  getCreatorWithOverlayByUsername,
+  getCreatorWithLinkTreeByUsername,
+} from "../services/creator.service.js";
 import ApiError from "../utils/error.api.js";
 import { ApiResponse } from "../utils/response.api.js";
+import catchAsync from "../utils/catchAsync.js";
 
-export const getCreatorController = async (req, res) => {
+export const getCreatorController = catchAsync(async (req, res) => {
   const { username } = req.params;
   const creator = await getFullCreatorByUsername(username);
   if (!creator) {
     throw new ApiError(404, "Creator not found");
   }
   res.json(new ApiResponse(200, creator));
-};
+});
 
-export const getCreatorWithOverlayController = async (req, res) => {
+export const getCreatorWithOverlayController = catchAsync(async (req, res) => {
   const { username } = req.params;
   const creator = await getCreatorWithOverlayByUsername(username);
   if (!creator) {
     throw new ApiError(404, "Creator not found");
   }
   res.json(new ApiResponse(200, creator));
-};
+});
 
-export const getCreatorWithLinkTreeController = async (req, res) => {
+export const getCreatorWithLinkTreeController = catchAsync(async (req, res) => {
   const { username } = req.params;
   const creator = await getCreatorWithLinkTreeByUsername(username);
   if (!creator) {
     throw new ApiError(404, "Creator not found");
   }
   res.json(new ApiResponse(200, creator));
-};
+});
